0e034f119b50377748290a930f8e007d80bdc64b
abidasobhan19 <abidasobhan19@gmail.com>
2020-08-24 23:43:13 -0700

    Updates

---
+++ precache-manifest.40a99da5e7ff454b5a592c6d35541ff2.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f22d2613dc0de96eda43e9e74c0cfec",
+    "url": "/Sultan_Tracker/index.html"
+  },
+  {
+    "revision": "743027091944510a4b98",
+    "url": "/Sultan_Tracker/static/css/2.f527e924.chunk.css"
+  },
+  {
+    "revision": "4bdffeacd9d1d1eada7d",
+    "url": "/Sultan_Tracker/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "743027091944510a4b98",
+    "url": "/Sultan_Tracker/static/js/2.3aa1ad8c.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/Sultan_Tracker/static/js/2.3aa1ad8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bdffeacd9d1d1eada7d",
+    "url": "/Sultan_Tracker/static/js/main.7922586e.chunk.js"
+  },
+  {
+    "revision": "9f4eaeee746503a539d1",
+    "url": "/Sultan_Tracker/static/js/runtime-main.48b5f8d0.js"
+  },
+  {
+    "revision": "f6ade3e94efa8e3fd87d621bb644c96f",
+    "url": "/Sultan_Tracker/static/media/abid.f6ade3e9.jpg"
+  },
+  {
+    "revision": "7fc858bf0dfeb9bfb8a347e7eb8754ef",
+    "url": "/Sultan_Tracker/static/media/alerts.7fc858bf.png"
+  },
+  {
+    "revision": "9a303e79db2b67c364340fcfa3a1ee24",
+    "url": "/Sultan_Tracker/static/media/bg1.9a303e79.png"
+  },
+  {
+    "revision": "56a46fd14a7a084ad5c23994d22fd898",
+    "url": "/Sultan_Tracker/static/media/fuel.56a46fd1.png"
+  },
+  {
+    "revision": "617dd1fe9a9e62ae3ef4cb289d47144f",
+    "url": "/Sultan_Tracker/static/media/live.617dd1fe.png"
+  },
+  {
+    "revision": "61cd43aadf6f239dd0ebf88ec218ddd6",
+    "url": "/Sultan_Tracker/static/media/live2.61cd43aa.png"
+  },
+  {
+    "revision": "307995d6ebf923aa85bda7f2a949a850",
+    "url": "/Sultan_Tracker/static/media/logo.307995d6.png"
+  },
+  {
+    "revision": "a7bda531eaf043fbe40a7afc114f4be1",
+    "url": "/Sultan_Tracker/static/media/milage.a7bda531.png"
+  },
+  {
+    "revision": "4183b6c40780f11add25109f1cce4bcc",
+    "url": "/Sultan_Tracker/static/media/photo.4183b6c4.webp"
+  },
+  {
+    "revision": "dc615a1b99a358f085099cb742440409",
+    "url": "/Sultan_Tracker/static/media/pro.dc615a1b.png"
+  },
+  {
+    "revision": "9dc8ab528939442646ed7dbba1d5020c",
+    "url": "/Sultan_Tracker/static/media/rev2.9dc8ab52.webp"
+  },
+  {
+    "revision": "5a6fa04cb9835694e2bd32d29219582e",
+    "url": "/Sultan_Tracker/static/media/rev3.5a6fa04c.webp"
+  },
+  {
+    "revision": "d0ae294dbf1a2c10c8db696b4a293b37",
+    "url": "/Sultan_Tracker/static/media/review.d0ae294d.webp"
+  },
+  {
+    "revision": "f183ed0ababc7272b55985a1e2a38253",
+    "url": "/Sultan_Tracker/static/media/saimul.f183ed0a.jpg"
+  },
+  {
+    "revision": "c04ba4dc422bd1ea62d07294066d175b",
+    "url": "/Sultan_Tracker/static/media/shamim.c04ba4dc.jpg"
+  },
+  {
+    "revision": "21c744ebbdaeb8f8cd0a01766afcd7c9",
+    "url": "/Sultan_Tracker/static/media/shohel.21c744eb.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Sultan_Tracker/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Sultan_Tracker/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Sultan_Tracker/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Sultan_Tracker/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "cb1918c73a0d942cf2bd6f46cabd81fd",
+    "url": "/Sultan_Tracker/static/media/speed.cb1918c7.png"
+  },
+  {
+    "revision": "6e3e19da32ff60a4de3c4c0f1dda8b43",
+    "url": "/Sultan_Tracker/static/media/summery.6e3e19da.png"
+  },
+  {
+    "revision": "b4fe5412e6e3c754fa923ed9b15ab7dc",
+    "url": "/Sultan_Tracker/static/media/traffic.b4fe5412.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Sultan_FrontEnd/precache-manifest.16e7aea959e7192496dd3e403555db07.js"
+  "/Sultan_Tracker/precache-manifest.40a99da5e7ff454b5a592c6d35541ff2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Sultan_FrontEnd/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Sultan_Tracker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
